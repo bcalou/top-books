feafc88464dd8a7ac2fd9798beadb33f49ee2647
bcalou <bastien.calou@gmail.com>
2021-03-31 14:58:37 +0200

    [ci] make gulp generate css directly inside _dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const clean = require('gulp-clean');
-const dist = './dist/';
+const dist = './_site/dist';
 
 function cleanDist() {
   return src(dist, { allowEmpty: true }).pipe(clean({ force: true }));
