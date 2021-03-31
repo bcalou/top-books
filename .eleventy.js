4a6dd0d634fd1ea87f12b314e155e567e0ac1f6c
bcalou <bastien.calou@gmail.com>
2021-03-31 18:14:47 +0200

    [global] move most of the code in the src folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection('booksAscending', (collection) =>
-    collection.getFilteredByGlob('books/*.md').sort((a, b) => {
+    collection.getFilteredByGlob('src/books/*.md').sort((a, b) => {
       if (a.data.title > b.data.title) return 1;
       else if (a.data.title < b.data.title) return -1;
       else return 0;
