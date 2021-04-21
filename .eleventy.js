0da2387485134180a43d30298ccfaafe9bfe1328
bcalou <bastien.calou@gmail.com>
2021-04-21 20:13:48 +0200

    [global] global optimization and design fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addCollection('booksAscending', (collection) =>
-    collection.getFilteredByGlob('src/books/*.md').sort((a, b) => {
+  eleventyConfig.addCollection('itemsAscending', (collection) =>
+    collection.getFilteredByGlob('src/items/*.md').sort((a, b) => {
       if (a.data.title > b.data.title) return 1;
       else if (a.data.title < b.data.title) return -1;
       else return 0;
